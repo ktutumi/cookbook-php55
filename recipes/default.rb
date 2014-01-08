bcbc228cf146daa1df908b9720214bffb8b61546
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2014-01-08 19:08:03 +0900

    php.ini 更新後にサービスをリロードする

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,5 +17,11 @@ template "#{node['php']['conf_dir']}/php.ini" do
   owner 'root'
   group 'root'
   mode 0644
+
+  %w{apache2 nginx}.each do |svc|
+    if node.recipe? svc
+      notifies :reload, "service[#{svc}]"
+    end
+  end
 end
 
